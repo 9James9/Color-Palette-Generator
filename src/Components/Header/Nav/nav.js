b0f3291be07ae7192af84350e445e44b2537e0ed
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-30 14:50:08 -0500

    added nav

---
+++ src/Components/Header/Nav/nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './nav.css'
+const Nav = () => {
+  return (
+    <div className='nav__container'>
+        <ul className='nav__items--container'>
+            <li>Home</li>
+            <li>Random color picker</li>
+            <li>Manual color picker</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './header.css'
+import Nav from './Nav/nav'
 const Header = () => {
   return (
     <div>
         <div className='header__container'>
             <h1 className='header__title'>Color Palette Generator</h1>
+            <nav>
+                <Nav/>
+            </nav>
         </div>
     </div>
   )
