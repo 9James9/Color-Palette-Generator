36cdd875900d201a1f86ee5dd969f4679c649d43
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-30 14:45:53 -0500

    installed react router dom and added header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import './App.css'
 import ColorGenerator from './Components/ColorGenerator/colorgenerator'
 import ColorPicker from './Components/ColorPicker/colorpicker'
+import Header from './Components/Header/header'
 function App() {
 
   return (
     <div className="app__container">
+      <Header/>
 <ColorPicker/>
     </div>
   )
---
+++ src/Components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+  return (
+    <div>
+        <div className='header__container'>
+            <h1 className='header__title'>Color Palette Generator</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
