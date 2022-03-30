0c5c9e995e9283719608996dbc5179065a74ce3a
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-30 14:54:30 -0500

    added router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
-import './App.css'
-import ColorGenerator from './Components/ColorGenerator/colorgenerator'
-import ColorPicker from './Components/ColorPicker/colorpicker'
-import Header from './Components/Header/header'
-function App() {
+import React, { useState } from "react";
+import "./App.css";
+import ColorGenerator from "./Components/ColorGenerator/colorgenerator";
+import ColorPicker from "./Components/ColorPicker/colorpicker";
+import Header from "./Components/Header/header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+function App() {
   return (
-    <div className="app__container">
-      <Header/>
-<ColorPicker/>
+    //     <div className="app__container">
+    //       <Header/>
+    // <ColorPicker/>
+    //     </div>
+    <div>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ColorPicker />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
