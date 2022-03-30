a124191a457c0d0293039b18685a088f26c6da9a
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-30 01:17:20 -0500

    clicking submit logs the color to the console

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [color, setColor] = useState(false)
   return (
-    <div>
-      <p>Clean React App</p>
+    <div className="container">
+      <div className='colorPicker'>
+        <form>
+          <input type="color" onChange={(e) => {
+            setColor(e.target.value)
+          }}></input>
+          <button onClick={(e) => {
+            e.preventDefault()
+
+            console.log(color)
+          }}>Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
