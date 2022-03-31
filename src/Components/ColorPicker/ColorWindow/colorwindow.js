46cbae7770c8c88185eac20730fa20372453a4a6
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-31 10:49:12 -0500

    fixed glitch where only one background will change

--- src/Components/ColorPicker/ColorWindow/colorwindow.js
+++ src/Components/ColorPicker/ColorWindow/colorwindow.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./colorwindow.css";
 const ColorWindow = () => {
   const [color, setColor] = useState("#696969");
@@ -8,17 +8,17 @@ const ColorWindow = () => {
   const copyColor = () => {
     navigator.clipboard.writeText(color);
   };
-  useEffect(() => {
-    document.querySelector(".colorWindow__container").style.backgroundColor =
-      color;
-  }, [color]);
+
+const backgroundStyle = {
+    backgroundColor: color
+}
   return (
     <div>
       <div className="inputContainer">
         <h4 className="colorWindow__Title">Select a color:</h4>
         <input type="color" onChange={updateColor} className="colorWindow__input"></input>
       </div>
-      <div className="colorWindow__container" onClick={copyColor}>
+      <div className="colorWindow__container" onClick={copyColor} style={backgroundStyle}>
         <p className="colorWindow__text">Copy to clipboard</p>
       </div>
     </div>
--- src/Components/ColorPicker/colorpicker.js
+++ src/Components/ColorPicker/colorpicker.js
@@ -1,19 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./colorpicker.css";
 import ColorWindow from "./ColorWindow/colorwindow";
 const ColorPicker = () => {
-    const [color, setColor] = useState('#fff')
-    useEffect(() => {
-        document.querySelector('.colorDisplay').style.backgroundColor = color
-    },[color])
-    const copyColorToClipboard = () => {
-        navigator.clipboard.writeText(color)
-    }
+
   return (
     <div className="container">
 
           <div className="colorDisplay">
-              <ColorWindow color={color} copy={copyColorToClipboard}/>
+              <ColorWindow/>
+              <ColorWindow/>
+              <ColorWindow/>
           </div>
 
       </div>
