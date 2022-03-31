be02d3ec605be115e4d78a94d6bec2abc6272d61
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-31 11:36:47 -0500

    added css to button

--- src/Components/ColorPicker/ColorWindow/colorwindow.js
+++ src/Components/ColorPicker/ColorWindow/colorwindow.js
@@ -8,15 +8,21 @@ const ColorWindow = () => {
   const copyColor = () => {
     navigator.clipboard.writeText(color);
   };
-
+const randomHex = () => {
+    let hex = Math.floor(Math.random()*16777215).toString(16)
+    return `#${hex}`
+}
 const backgroundStyle = {
     backgroundColor: color
 }
   return (
     <div>
       <div className="inputContainer">
-        <h4 className="colorWindow__Title">Select a color:</h4>
         <input type="color" onChange={updateColor} className="colorWindow__input"></input>
+        <button className="colorWindow__randomBtn" onClick={(e) => {
+              e.preventDefault()
+              setColor(randomHex)
+          }}>Random Color</button>
       </div>
       <div className="colorWindow__container" onClick={copyColor} style={backgroundStyle}>
         <p className="colorWindow__text">Copy to clipboard</p>
