17d5f67f36b9a6b7db211d2d1025256fe3340aa8
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-30 20:01:55 -0500

    added color input to each colorwindow component

--- src/Components/ColorPicker/ColorWindow/colorwindow.js
+++ src/Components/ColorPicker/ColorWindow/colorwindow.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./colorwindow.css"
 const ColorWindow = () => {
+    const [color, setColor] = useState('#fff')
+    const updateColor = e => {
+        setColor(e.target.value)
+    }
+    const copyColor = () => {
+        
+    }
+    useEffect(() => {
+        document.querySelector('.colorWindow__container').style.backgroundColor = color
+    },[color])
   return (
     <div>
         <div className='colorWindow__container'>
-            <h3>Container!</h3>
-
+            <p className='colorWindow__text'>hello world</p>
+        </div>
+        <div className='inputContainer'>
+            <h4>Select a color:</h4>
+            <input type="color" onChange={updateColor}></input>
         </div>
     </div>
   )
