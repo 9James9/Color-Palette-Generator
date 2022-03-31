93e4205ec442924b1875e2a1595b0f6455d8a8eb
James Driscoll <thejamesdriscoll@gmail.com>
2022-03-30 20:05:54 -0500

    moved color picker to top

--- src/Components/ColorPicker/ColorWindow/colorwindow.js
+++ src/Components/ColorPicker/ColorWindow/colorwindow.js
@@ -1,27 +1,28 @@
-import React, {useState, useEffect} from 'react'
-import "./colorwindow.css"
+import React, { useState, useEffect } from "react";
+import "./colorwindow.css";
 const ColorWindow = () => {
-    const [color, setColor] = useState('#fff')
-    const updateColor = e => {
-        setColor(e.target.value)
-    }
-    const copyColor = () => {
-        
-    }
-    useEffect(() => {
-        document.querySelector('.colorWindow__container').style.backgroundColor = color
-    },[color])
+  const [color, setColor] = useState("#696969");
+  const updateColor = (e) => {
+    setColor(e.target.value);
+  };
+  const copyColor = () => {
+    navigator.clipboard.writeText(color);
+  };
+  useEffect(() => {
+    document.querySelector(".colorWindow__container").style.backgroundColor =
+      color;
+  }, [color]);
   return (
     <div>
-        <div className='colorWindow__container'>
-            <p className='colorWindow__text'>hello world</p>
-        </div>
-        <div className='inputContainer'>
-            <h4>Select a color:</h4>
-            <input type="color" onChange={updateColor}></input>
-        </div>
+      <div className="inputContainer">
+        <h4>Select a color:</h4>
+        <input type="color" onChange={updateColor}></input>
+      </div>
+      <div className="colorWindow__container" onClick={copyColor}>
+        <p className="colorWindow__text">Copy to clipboard</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ColorWindow
+export default ColorWindow;
