c7e300ddf7fff2c809e459dea6a04bd6a7787b1b
James Driscoll <thejamesdriscoll@gmail.com>
2022-04-25 00:54:07 -0500

    called randomcolors on pageload with useeffect

--- src/Components/ColorPicker/colorpicker.js
+++ src/Components/ColorPicker/colorpicker.js
@@ -1,8 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./colorpicker.css";
 import ColorWindow from "./ColorWindow/colorwindow";
 const ColorPicker = () => {
   const [color, setColor] = useState(false)
+  useEffect(() => {
+    randomColors()
+  },[])
   const randomHex = () => {
     let hex = Math.floor(Math.random()*16777215).toString(16)
     return `#${hex}`
