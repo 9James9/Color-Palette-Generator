9aed2de4746b34439fa3cac56260d2d1aca24990
James Driscoll <thejamesdriscoll@gmail.com>
2022-04-25 00:33:33 -0500

    added colors to container and header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -2,15 +2,21 @@ import React from 'react'
 import './header.css'
 import Nav from './Nav/nav'
 const Header = () => {
+//   const randomHex = () => {
+//     let hex = Math.floor(Math.random()*16777215).toString(16)
+//     navigator.clipboard.writeText(`#${hex}`);
+//     return `#${hex}`
+// }
+//   const styleHeader = () => {
+//     return document.querySelector('.header__container').style.background = randomHex()
+//   }
   return (
-    <div>
         <div className='header__container'>
             <h1 className='header__title'>Color Palette Generator</h1>
             <nav className='nav__container'>
                 <Nav/>
             </nav>
         </div>
-    </div>
   )
 }
 
