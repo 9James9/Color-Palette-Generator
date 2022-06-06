bd841534c3d6afe4de71bdad20dda668151abb8a
James Driscoll <thejamesdriscoll@gmail.com>
2022-06-06 09:31:45 -0700

    added some text to home page

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -2,7 +2,11 @@ import React from 'react'
 import './home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home__container'>
+      <h1>Welcome to my color picker</h1>
+      <p>Idk why I made a home page but I should probably put some text here. Maybe I'll add some more stuff to this page eventually.</p>
+      <p>Click "color picker" to get to the good stuff</p>
+    </div>
   )
 }
 
