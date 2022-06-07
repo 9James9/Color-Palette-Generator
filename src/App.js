2b698d86ed743debc064a0bbef251a6fc7d92867
James Driscoll <thejamesdriscoll@gmail.com>
2022-06-07 11:39:29 -0500

    removed unused state import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Saved from "./Components/Saved/saved";
 import ColorPicker from "./Components/ColorPicker/colorpicker";
